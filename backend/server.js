4cbd4e3dbdf9cc590e404e580f12db7896bc29af
Meliza <meaniemel@MacAir.local>
2025-03-23 21:01:48 +1000

    EMS-2 Cleaned up files

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/employees', require('./routes/employeeRoutes'));
 
 // Export the app object for testing
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
 import Employees from './pages/Employee';
 
 function App() {
@@ -15,9 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={ !user ? <Login /> : <Navigate to="/employees"/>} />
         <Route path="/login" element={ !user ? <Login /> : <Navigate to="/employees"/>} />
-        <Route path="/register" element={ <Register />} />
+        <Route path="/register" element={ user ?  <Register /> : <Navigate to="/login" />} />
         <Route path="/profile" element={ user ? <Profile /> : <Navigate to="/login" />} />
-        <Route path="/tasks" element={ user ? <Tasks /> : <Navigate to="/login" />} />
         <Route path="/employees" element={ user ? <Employees /> : <Navigate to="/login" />}/>
       </Routes>
     </Router>
